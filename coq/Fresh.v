952620b4d301033d4198dc9c9d1a2b0dbb99ea74
Matthis Kruse <matthis.kruse@cispa.de>
2022-08-31 17:08:59 +0200

    Define pstep functionally and show equivalence with existing, relational definition.

--- coq/Fresh.v
+++ coq/Fresh.v
@@ -1,8 +1,10 @@
 Set Implicit Arguments.
+Require Import Strings.String.
 
 Record fresh_state := { counter : nat }.
 
 Definition empty_fresh := {| counter := 0 |}.
 Definition fresh (x : fresh_state) : nat := x.(counter).
+Definition freshv (x : fresh_state) : string := "x"%string.
 Definition advance (x : fresh_state) : fresh_state := {| counter := x.(counter) + 1 |}.
 
--- coq/Langs/S.v
+++ coq/Langs/S.v
@@ -1,6 +1,6 @@
 Set Implicit Arguments.
 Require Import Strings.String Lists.List Program.Equality.
-Require Import CSC.Sets CSC.Util.
+Require Import CSC.Sets CSC.Util CSC.Fresh.
 
 (** * Syntax *)
 
@@ -101,12 +101,12 @@ Definition fresh_tvar := @fresh string String.eqb.
 Definition eval_binop (b : binopsymb) (v0 v1 : value) :=
   let '(Vnat n0) := v0 in
   let '(Vnat n1) := v1 in
-  match b with
-  | Bless => Vnat (if Nat.ltb n0 n1 then 0 else 1)
-  | BAdd => Vnat (n0 + n1)
-  | BSub => Vnat (n0 - n1)
-  | BMul => Vnat (n0 * n1)
-  end
+  Vnat(match b with
+       | Bless => (if Nat.ltb n0 n1 then 0 else 1)
+       | BAdd => (n0 + n1)
+       | BSub => (n0 - n1)
+       | BMul => (n0 * n1)
+       end)
 .
 (** Poison used to mark locations in our operational state. *)
 Inductive poison : Type :=
@@ -147,6 +147,10 @@ Fixpoint append (Δ1 Δ2 : store) : store :=
 .
 (* '◘' is `\inversebullet` *)
 Notation "Δ1 '◘' Δ2" := (append Δ1 Δ2) (at level 82, Δ2 at next level).
+(** Splitting a store in three parts. *)
+Fixpoint splitat (Δ : store) (x : vart) :=
+  None : option (store * vart * dynloc * store)
+.
 (** In this model, heaps are just a snoc-style list of natural numbers. *)
 Inductive heap : Type :=
 | Hnil : heap
@@ -187,8 +191,8 @@ Fixpoint Hgrow (H : heap) (s : nat) : heap :=
   | S s' => Hcons (Hgrow H s') 0
   end
 .
-Definition state : Type := heap * store.
-Notation "H ';' Δ" := ((H : heap), (Δ : store)) (at level 81).
+Definition state : Type := CSC.Fresh.fresh_state * heap * store.
+Notation "F ';' H ';' Δ" := ((F : CSC.Fresh.fresh_state), (H : heap), (Δ : store)) (at level 81, H at next level, Δ at next level).
 
 Inductive context (e0 e1 : expr) : Type := Ccontext.
 Inductive component (ep : expr) : Type := Ccomponent.
@@ -272,25 +276,230 @@ Inductive pstep : rtexpr -> event -> rtexpr -> Prop :=
     Ω ▷ Xifz (S n) e1 e2 --[ Sε ]--> Ω ▷ e2
 | e_abort : forall (Ω : state),
     Ω ▷ Xabort --[ Scrash ]--> ↯ ▷ stuck
-| e_get : forall (H : heap) (Δ1 Δ2 : store) (x : vart) (ℓ n v : nat) (ρ : poison),
+| e_get : forall (F : CSC.Fresh.fresh_state) (H : heap) (Δ1 Δ2 : store) (x : vart) (ℓ n v : nat) (ρ : poison),
     forall (H0a : ℓ + n < Hsize H -> Some v = Hget H (ℓ + n))
       (H0b : ℓ + n >= Hsize H -> v = 1729),
-    (H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ Xget x n --[ Sget (addr ℓ) n ]--> (H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ v
-| e_set : forall (H H' : heap) (Δ1 Δ2 : store) (x : vart) (ℓ n v : nat) (ρ : poison),
+    (F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ Xget x n --[ Sget (addr ℓ) n ]--> (F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ v
+| e_set : forall (F : CSC.Fresh.fresh_state) (H H' : heap) (Δ1 Δ2 : store) (x : vart) (ℓ n v : nat) (ρ : poison),
     forall (H0a : ℓ + n < Hsize H -> Some H' = Hset H (ℓ + n) v)
       (H0b : ℓ + n >= Hsize H -> H' = H),
-    (H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ Xset x n v --[ Sget (addr ℓ) n ]--> (H' ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ v
-| e_delete : forall (H : heap) (Δ1 Δ2 : store) (x : vart) (ℓ : nat) (ρ : poison),
-    (H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ Xdel x --[ Sdealloc (addr ℓ) ]--> (H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ☣) ◘ Δ2)) ▷ 0
-| e_alloc : forall (H H' : heap) (Δ : store) (x z : vart) (ℓ n : nat) (e : expr),
-    fresh_loc (Δimg Δ) (addr ℓ) ->
-    fresh_tvar (Δdom Δ) z ->
+    (F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ Xset x n v --[ Sset (addr ℓ) n ]--> (F ; H' ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ v
+| e_delete : forall (F : CSC.Fresh.fresh_state) (H : heap) (Δ1 Δ2 : store) (x : vart) (ℓ : nat) (ρ : poison),
+    (F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2)) ▷ Xdel x --[ Sdealloc (addr ℓ) ]--> (F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ☣) ◘ Δ2)) ▷ 0
+| e_let : forall (Ω : state) (x : vart) (f : fnoerr) (e e' : expr),
+    e' = subst x f e ->
+    Ω ▷ Xlet x f e --[ Sε ]--> Ω ▷ e'
+| e_alloc : forall (F F' F'' : CSC.Fresh.fresh_state) (H H' : heap) (Δ : store) (x z : vart) (ℓ n : nat) (e : expr),
+    ℓ = Fresh.fresh F ->  F' = Fresh.advance F ->
+    z = Fresh.freshv F' -> F'' = Fresh.advance F' ->
+    (*TODO: fresh_loc (Δimg Δ) (addr ℓ) ->*)
+    (*fresh_tvar (Δdom Δ) z ->*)
     H' = Hgrow H n ->
-    (H ; Δ) ▷ Xnew x n e --[ Salloc (addr ℓ) n ]--> (H' ; (z ↦ ((addr ℓ) ⋅ ◻) ◘ Δ)) ▷ (subst x (Fvar z) e)
+    (F ; H ; Δ) ▷ Xnew x n e --[ Salloc (addr ℓ) n ]--> (F'' ; H' ; (z ↦ ((addr ℓ) ⋅ ◻) ◘ Δ)) ▷ (subst x (Fvar z) e)
 where "r0 '--[' a ']-->' r1" := (pstep r0 a r1)
-.
+. (*TODO: incorporate freshness*)
 #[global]
 Hint Constructors pstep : core.
+
+(** functional version of the above *)
+Definition pstepf (r : rtexpr) : option (event * rtexpr) :=
+  let '(oΩ, e) := r in
+  match oΩ with
+  | None => None
+  | Some Ω =>
+    match e with
+    | Xreturning F => (* e-returning *)
+      match F with
+      | Xres(Fres f) =>
+        Some(Sret f, Ω ▷ F)
+      | _ => None
+      end
+    | Xcalling F => (* e-calling *)
+      match F with
+      | Xres(Fres f) =>
+        Some(Scall f, Ω ▷ F)
+      | _ => None
+      end
+    | Xbinop b n1 n2 => (* e-binop *)
+      match n1, n2 with
+      | Xres(Fres(Fval(Vnat m1))), Xres(Fres(Fval(Vnat m2))) =>
+        let n3 := eval_binop b m1 m2 in
+        Some(Sε, Ω ▷ n3)
+      | _, _ => None
+      end
+    | Xifz 0 e1 e2 => (* e-ifz-true *)
+      Some(Sε, Ω ▷ e1)
+    | Xifz (S _) e1 e2 => (* e-ifz-false *)
+      Some(Sε, Ω ▷ e2)
+    | Xabort => (* e-abort *)
+      Some(Scrash, ↯ ▷ stuck)
+    | Xget x en => (* e-get *)
+      match en with
+      | Xres(Fres(Fval(Vnat n))) =>
+        let '(F, H, Δ) := Ω in
+        match splitat Δ x with
+        | None => None
+        | Some(Δ1, x, (L, ρ), Δ2) =>
+          let '(addr ℓ) := L in
+          let v := match Hget H (ℓ + n) with
+                  | None => 1729
+                  | Some w => w
+                  end
+          in
+            Some(Sget (addr ℓ) n, F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2) ▷ v)
+        end
+      | _ => None
+      end
+    | Xset x en ev => (* e-set *)
+      match en, ev with
+      | Xres(Fres(Fval(Vnat n))), Xres(Fres(Fval(Vnat v))) =>
+        let '(F, H, Δ) := Ω in
+        match splitat Δ x with
+        | None => None
+        | Some(Δ1, x, (L, ρ), Δ2) =>
+          let '(addr ℓ) := L in
+          match Hset H (ℓ + n) v with
+          | Some H' =>
+            Some(Sset (addr ℓ) n, F ; H' ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2) ▷ v)
+          | None =>
+            Some(Sset (addr ℓ) n, F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ρ) ◘ Δ2) ▷ v)
+          end
+        end
+      | _, _ => None
+      end
+    | Xdel x => (* e-delete *)
+      let '(F, H, Δ) := Ω in
+      match splitat Δ x with
+      | None => None
+      | Some(Δ1, x, (L, ρ), Δ2) =>
+        let '(addr ℓ) := L in
+        Some(Sdealloc (addr ℓ), F ; H ; (Δ1 ◘ x ↦ ((addr ℓ) ⋅ ☣) ◘ Δ2) ▷ 0)
+      end
+    | Xlet x ef e' => (* e-let *)
+      match ef with
+      | Xres(Fres f) =>
+        let e'' := subst x f e' in
+        Some(Sε, Ω ▷ e'')
+      | _ => None
+      end
+    | Xnew x ef e' => (* e-new *)
+      match ef with
+      | Xres(Fres(Fval(Vnat n))) =>
+        let '(F, H, Δ) := Ω in
+        let H' := Hgrow H n in
+        let ℓ := CSC.Fresh.fresh F in
+        let F' := Fresh.advance F in
+        let z := CSC.Fresh.freshv F' in
+        let e'' := subst x (Fvar z) e' in
+        Some(Salloc (addr ℓ) n, Fresh.advance F' ; H' ; (z ↦ ((addr ℓ) ⋅ ◻) ◘ Δ) ▷ e'')
+      | _ => None
+      end
+    | _ => None (* no matching rule *)
+    end
+  end
+.
+(** We show that the functional style semantics an dthe relational style are equivalent. *)
+Ltac crush_interp :=
+    match goal with
+    | [H: match ?oΩ with | Some _ => _ | None => None end = Some _ |- _] =>
+        let Ω := fresh "Ω" in destruct oΩ as [|]; inversion H
+    end.
+Ltac grab_value e :=
+  (destruct e as [[[[e]|]|]| | | | | | | | | | |]; try congruence)
+.
+Ltac grab_value2 e1 e2 := (grab_value e1; grab_value e2).
+Ltac grab_final e :=
+  (destruct e as [[e|]| | | | | | | | | | | ]; try congruence)
+.
+
+Lemma splitat_elim Δ1 Δ2 x ℓ ρ :
+  splitat (Δ1 ◘ Scons x (addr ℓ, ρ) Δ2) x = Some (Δ1, x, (addr ℓ, ρ), Δ2).
+Proof. Admitted.
+Lemma splitat_base Δ x :
+  splitat Δ x <> None -> exists Δ1 ℓ ρ Δ2, Δ = (Δ1 ◘ x ↦ (ℓ ⋅ ρ) ◘ Δ2).
+Proof. Admitted.
+Lemma splitat_none_or_not_none Δ x :
+  splitat Δ x = None \/ splitat Δ x <> None.
+Proof. Admitted.
+Lemma Hget_none H n :
+  n >= Hsize H -> Hget H n = None.
+Proof. Admitted.
+Lemma Hget_some H n :
+  n < Hsize H -> exists v, Hget H n = Some v.
+Proof. Admitted.
+Lemma Hset_none H n v :
+  n >= Hsize H -> Hset H n v = None.
+Proof. Admitted.
+Lemma Hset_some H n v :
+  n < Hsize H -> exists H', Hset H n v = Some H'.
+Proof. Admitted.
+
+Lemma equiv_pstep r0 a r1 :
+  r0 --[ a ]--> r1 <-> pstepf r0 = Some(a, r1).
+Proof.
+  split.
+  - induction 1.
+    + (* e-returning *) now cbn.
+    + (* e-calling *) now cbn.
+    + (* e-binop *) rewrite H; now cbn.
+    + (* e-ifz-true *) now cbn.
+    + (* e-ifz-false *) now cbn.
+    + (* e-abort *) now cbn.
+    + (* e-get *) cbn.
+      destruct (Arith.Compare_dec.lt_dec (ℓ + n) (Hsize H)) as [H1a | H1b]; rewrite splitat_elim.
+      * now specialize (H0a H1a) as H0a'; inv H0a'.
+      * apply Arith.Compare_dec.not_lt in H1b; specialize (H0b H1b) as H1b'.
+        now rewrite (@Hget_none H (ℓ + n) H1b); subst.
+    + (* e-set *) cbn.
+      destruct (Arith.Compare_dec.lt_dec (ℓ + n) (Hsize H)) as [H1a | H1b]; rewrite splitat_elim.
+      * now rewrite <- (H0a H1a).
+      * apply Arith.Compare_dec.not_lt in H1b; specialize (H0b H1b) as H1b'; subst.
+        now rewrite (@Hset_none H (ℓ + n) v H1b).
+    + (* e-delete *) now cbn; rewrite splitat_elim.
+    + (* e-let *) now subst; cbn.
+    + (* e-new *) now rewrite H3; subst; cbn.
+  - intros H; destruct r0 as [oΩ e], r1 as [Ω' e']; destruct e; cbn in H; crush_interp; clear H.
+    + (* e = e1 ⊕ e2 *)
+      now grab_value2 e1 e2; inv H1; eapply e_binop.
+    + (* e = x[e] *)
+      grab_value e. destruct s as [[F H] Δ].
+      destruct (splitat_none_or_not_none Δ x) as [H0|H0]; try (rewrite H0 in H1; congruence).
+      apply splitat_base in H0 as [Δ1 [[ℓ] [ρ [Δ2 H0]]]]. rewrite H0 in H1.
+      rewrite splitat_elim in H1. inv H1. eapply e_get; intros H0.
+      * now apply Hget_some in H0 as [v ->].
+      * now apply Hget_none in H0 as ->.
+    + (* e = x[e1] <- e2 *)
+      grab_value2 e1 e2. destruct s as [[F H] Δ].
+      destruct (splitat_none_or_not_none Δ x) as [H0|H0]; try (rewrite H0 in H1; congruence).
+      apply splitat_base in H0 as [Δ1 [[ℓ] [ρ [Δ2 H0]]]]. rewrite H0 in H1.
+      rewrite splitat_elim in H1.
+      destruct (Arith.Compare_dec.lt_dec (ℓ + e1) (Hsize H)) as [H2|H2].
+      * apply (@Hset_some H (ℓ + e1) e2) in H2 as [H' H2]. rewrite H2 in H1.
+        inv H1. eapply e_set; intros H0; subst; try easy.
+        eapply (@Hset_none H (ℓ + e1) e2) in H0; congruence.
+      * apply Arith.Compare_dec.not_lt in H2. apply (@Hset_none H (ℓ + e1) e2) in H2; subst; rewrite H2 in H1.
+        inv H1. eapply e_set; intros H0; try easy. apply (@Hset_some H (ℓ + e1) e2) in H0 as [H' H0]; congruence.
+    + (* e = let x = e1 in e2 *)
+      grab_final e1; inv H1; now eapply e_let.
+    + (* e = let x = new e1 in e2 *)
+      grab_value e1; destruct s as [[F H] Δ]; inv H1; eapply e_alloc; eauto.
+    + (* e = delete x *)
+      destruct s as [[F H] Δ]; inv H1.
+      destruct (splitat_none_or_not_none Δ x) as [H0|H0]; try (rewrite H0 in H2; congruence).
+      apply splitat_base in H0 as [Δ1 [[ℓ] [ρ [Δ2 H0]]]]. rewrite H0 in H2.
+      rewrite splitat_elim in H2; subst. inv H2. apply e_delete.
+    + (* e = returning e *)
+      grab_final e; inv H1; apply e_returning.
+    + (* e = calling e *)
+      grab_final e; inv H1; apply e_calling.
+    + (* e = ifz c e0 e1 *)
+      grab_value e1. destruct e1; inv H1; apply e_ifz_true || apply e_ifz_false.
+    + (* e = abort *)
+      apply e_abort.
+Qed.
+
+(* TODO: do the above for estep and starstep, too *)
+
 
 (** We proceed to define the dynamic semantics via evaluation contexts/environments. *)
 Inductive evalctx : Type :=
@@ -383,6 +592,8 @@ Inductive fill_j : list string -> fill_placeholder -> expr -> expr -> expr -> Pr
 | fillQ : forall e0 e1 e2, fill FP_Q e0 e1 = e2 -> fill_j nil FP_Q e0 e1 e2
 | fillN : forall q e0 e1 e2 z X, fresh_tvar X z -> q = FP_N z -> fill q e0 e1 = e2 -> fill_j X q e0 e1 e2
 .
+#[global]
+Hint Constructors fill_j : core.
 
 (** Evaluation of programs *)
 (*TODO: add typing*)
@@ -394,9 +605,46 @@ Inductive wstep (e0 ep e1 : expr) : prog e0 ep e1 -> tracepref -> rtexpr -> Prop
               (As0 Asp As0' As1 : tracepref),
     fill_j nil FP_Q ep e0 e0' ->
     (* typing -> *)
-    (Hnil ; Snil ▷ e0' ==[ As0 · Scall f0 ]==>* Ω0 ▷ ep0) ->
+    (Fresh.empty_fresh ; Hnil ; Snil ▷ e0' ==[ As0 · Scall f0 ]==>* Ω0 ▷ ep0) ->
     (Ω0 ▷ ep0 ==[ Asp · Sret fp ]==>* Ωp0 ▷ e0'') ->
     (Ωp0 ▷ e0'' ==[ As0' ]==>* Ω0' ▷ fp') ->
     (Ω0' ▷ (subst y e1 fp') ==[ As1 ]==>* Ω1 ▷ f1) ->
     wstep (Cprog e0 ep e1) (Sret 0 · As0 · Scall f0 · Asp · Sret fp · As0' · As1 · Scall f1) (Ω1 ▷ f1)
 .
+Notation "'PROG[' e0 '][' ep '][' e1 ']====[' As ']===>' r" := (wstep (Cprog e0 ep e1) As r) (at level 81, r at next level).
+
+Definition cmptrpref {e0 ep e1 : expr} (p : prog e0 ep e1) :=
+  exists As Ω f, PROG[e0][ep][e1]====[ As ]===> (Ω ▷ f)
+.
+
+(* let z = new x in let w = x[1337] in let _ = delete z in w*)
+Definition smsunsafe_ep : expr :=
+  Xnew "z"%string
+       (Fvar "x"%string)
+       (Xlet "w"%string
+             (Xget "x"%string 1337)
+             (Xlet "_"%string
+                   (Xdel "z"%string)
+                   (Fvar "w"%string))
+       )
+.
+(* let x = 42 in hole : (x : nat) -> nat *)
+Definition smsunsafe_e0 : expr :=
+  Xlet "x"%string
+       42
+       (Xhole "x"%string Tnat Tnat)
+.
+(* y *)
+Definition smsunsafe_e1 : expr :=
+  Fvar "y"%string
+.
+
+Definition smsunsafe_prog := Cprog smsunsafe_e0 smsunsafe_ep smsunsafe_e1.
+
+(*TODO: use functional-style interpreters to get concrete trace via simple `cbn` *)
+Goal cmptrpref smsunsafe_prog.
+Proof.
+  unfold cmptrpref. do 3 eexists.
+  eapply e_wprog; eauto; cbn.
+
+Admitted.

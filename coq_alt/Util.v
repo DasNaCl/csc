7bce010e2e3a8b5df7f78b132a479c4c57efe914
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2023-07-02 17:06:46 +0200

    :chicken: Refactor

--- coq_alt/Util.v
+++ coq_alt/Util.v
@@ -1040,6 +1040,152 @@ Ltac elim_split :=
 #[export]
 Hint Resolve eqb_eq String.eqb_refl : core.
 
+Lemma in_propagate {A : Type} l0 l (L0 L1 : list A) :
+  List.In l (L0 ++ l0 :: L1)%list ->
+  l <> l0 ->
+  List.In l (L0 ++ L1)%list
+.
+Proof.
+  induction L0; cbn; intros H H0.
+  - destruct H; subst; easy.
+  - destruct H.
+    + now left.
+    + right. now apply IHL0.
+Qed.
+Lemma notin_propagate {A : Type} l0 l (L0 L1 : list A) :
+  ~List.In l (L0 ++ l0 :: L1)%list ->
+  ~List.In l (L0 ++ L1)%list
+.
+Proof.
+  induction L0; cbn; intros H H0.
+  - apply H; now right.
+  - destruct H0 as [H0 | H0].
+    + apply H; now left.
+    + apply IHL0; auto.
+Qed.
+Lemma notin_cons {A : Type} l0 l (L : list A) :
+  ~ List.In l0 L ->
+  l <> l0 ->
+  ~ List.In l0 (L ++ l::nil)%list
+.
+Proof.
+  induction L; cbn; intros H H0 H1.
+  - now destruct H1.
+  - destruct H1.
+    + apply H; now left.
+    + apply IHL; auto.
+Qed.
+Lemma notin_unsnoc {A : Type} l0 l (L : list A) :
+  ~ List.In l0 (L ++ l :: nil)%list  ->
+  l <> l0 ->
+  ~ List.In l0 L
+.
+Proof.
+  induction L; cbn; intros H H0 H1.
+  - exact H1.
+  - destruct H1.
+    + apply H. now left.
+    + apply IHL; auto.
+Qed.
+Lemma in_unsnoc {A : Type} l0 l (L : list A) :
+  List.In l0 L ->
+  l <> l0 ->
+  List.In l0 (L ++ l :: nil)%list
+.
+Proof.
+  induction L; cbn; intros H H0.
+  - easy.
+  - destruct H.
+    + now left.
+    + right; apply IHL; auto.
+Qed.
+
+Fixpoint opt { A : Type } (As : list A) : list(option A) :=
+  match As with
+  | nil => nil
+  | cons a As => Some a :: opt As
+  end
+.
+Lemma opt_nil { A : Type } (As : list A) :
+  opt As = nil ->
+  As = nil
+.
+Proof. now induction As. Qed.
+Lemma opt_cons { A : Type } (As : list A) Bs (a : option A) :
+  opt As = (a :: Bs)%list ->
+  As <> nil
+.
+Proof. induction As; now cbn. Qed.
+Lemma opt_some { A : Type } (As : list A) Bs (a : option A) :
+  opt As = (a :: Bs)%list ->
+  exists a' As', a = Some a' /\ (As = a' :: As')%list /\ Bs = opt As'
+.
+Proof.
+  intros H; apply opt_cons in H as H'.
+  revert a Bs H; induction As; try congruence; intros.
+  cbn in H. inv H. exists a. exists As. repeat split; reflexivity.
+Qed.
+Fixpoint zip { A B : Type } (As : list A) (Bs : list B) : option (list (A * B)) :=
+  match As, Bs with
+  | nil, nil => Some(nil)
+  | (a :: As')%list, (b :: Bs')%list =>
+    let* (xs) := zip As' Bs' in
+    Some ((a, b) :: xs)%list
+  | _, _ => None
+  end
+.
+Lemma zip_empty { A B : Type } (As : list A) (Bs : list B) :
+  zip As Bs = Some nil ->
+  As = nil /\ Bs = nil
+.
+Proof.
+  destruct As; intros H.
+  - now destruct Bs.
+  - destruct Bs. inv H. cbn in H.
+    change ((fun xz => match xz with
+            | Some x => Some ((a, b ) :: x)%list
+            | None => None
+            end = Some nil) (zip As Bs)) in H.
+    destruct (zip As Bs); inv H.
+Qed.
+Lemma zip_cons { A B : Type } (As : list A) (Bs : list B) xs (a : A) (b : B) :
+  zip As Bs = Some ((a,b) :: xs)%list ->
+  exists As' Bs', As = (a :: As')%list /\ Bs = (b :: Bs')%list /\ Some xs = zip As' Bs'
+.
+Proof.
+  destruct As; intros H.
+  - now destruct Bs.
+  - destruct Bs. inv H. cbn in H.
+    assert (H':=H);
+    change ((fun xz => match xz with
+            | Some x => Some((a0, b0) :: x)%list
+            | None => None
+            end = Some ((a, b) :: xs)%list) (zip As Bs)) in H.
+    destruct (zip As Bs) in H; inv H.
+    exists As. exists Bs. repeat split; eauto.
+    remember (zip As Bs) as ys.
+    crush_option ys. now inv H'.
+Qed.
+Lemma zip_singleton { A B : Type } (As : list A) (Bs : list B) (a : A) (b : B) :
+  zip As Bs = Some ((a, b) :: nil)%list ->
+  (As = a :: nil /\ Bs = b :: nil)%list
+.
+Proof.
+  intros.
+  apply zip_cons in H. deex; destruct H as [H1 [H2 H3]]; subst.
+  symmetry in H3; apply zip_empty in H3 as [H3a H3b]; subst; easy.
+Qed.
+Lemma zip_opt_extend { A B : Type } (As : list A) (Bs : list B) Cs (a : A) (b : B) :
+  zip (opt As) (opt Bs) = Some Cs ->
+  zip (opt (a :: As)) (opt (b :: Bs)) = Some ((Some a, Some b) :: Cs)%list
+.
+Proof.
+  revert As Bs a b; induction Cs; intros.
+  - apply zip_empty in H as [Ha Hb]; cbn; subst. now rewrite Ha, Hb; cbn.
+  - destruct a as [a' b']. apply zip_cons in H. deex. destruct H as [H1 [H2 H3]].
+    cbn; rewrite H1, H2; cbn; now rewrite <- H3.
+Qed.
+
 (** Typeclass to define trace model *)
 Class TraceParams : Type := {
   Ev : Type ;

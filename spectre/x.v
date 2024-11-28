4f81e7e2911045ecf99f505dc3d202a04a7a808c
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2024-11-28 15:26:03 +0100

    Fix typing of fencing

--- spectre/x.v
+++ spectre/x.v
@@ -17,11 +17,38 @@ Definition abstraction (A B : Type) (rel : xrelation A B) : (B -> Prop) -> (A ->
 Definition concretization (A B : Type) (rel : xrelation A B) : (A -> Prop) -> (B -> Prop) :=
   fun π b => exists a, rel a b /\ π a
 .
+Definition subsets (A : Type) (X Y : A -> Prop) : Prop :=
+  forall x, X x -> Y x
+.
+Definition galois_connection (A B : Type) (σ : (B -> Prop) -> (A -> Prop)) 
+                                          (τ : (A -> Prop) -> (B -> Prop)) : Prop :=
+  forall (x : A -> Prop) (y : B -> Prop), subsets _ (τ x) y <-> subsets _ x (σ y)
+.
+Lemma isconn (A B : Type) (rel : xrelation A B) :
+  galois_connection A B (abstraction A B rel) (concretization A B rel)
+.
+Proof.
+  intros X Y; split; intros Hsub y Hy.
+  - intros x Hx; apply Hsub; exists y; now split.
+  - destruct Hy as [x [Hrel HXx]]; apply Hsub with (x:=x); assumption.
+Qed.
+Lemma galois1 (A B : Type) (rel : xrelation A B) :
+  forall (π : A -> Prop), subsets _ π (abstraction A B rel (concretization A B rel π))
+.
+Proof.
+  intros π x Hx y Hxy; now exists x.
+Qed.
+Lemma galois2 (A B : Type) (rel : xrelation A B) :
+  forall (π : B -> Prop), subsets _ (concretization A B rel (abstraction A B rel π)) π
+.
+Proof.
+  intros π x [y [Hyx Habs]]; apply Habs; assumption.
+Qed.
 Definition galois_insertion (A B : Type) (rel : xrelation A B) : Prop :=
   forall π, abstraction A B rel (concretization A B rel π) = π
 .
 Axiom set_exteq : forall (A : Type) (X Y : A -> Prop), 
-  ((forall x, X x -> Y x) /\ (forall y, Y y -> X y)) ->
+  (subsets A X Y /\ subsets A Y X) ->
   X = Y
 .
 Lemma insertion (A B : Type) (rel : xrelation A B) :
